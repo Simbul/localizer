837b027500299a39db2127d97d68cbe01ae3c2cf
Simbul <webmaster@simbul.net>
2010-05-28 16:51:22 +0200

    Added migration generator.

---
+++ generators/localizer/localizer_generator.rb
@@ -0,0 +1,15 @@
+class ˆLocalizerGenerator < Rails::Generator::Base
+  attr_accessor :migration_name
+ 
+  def manifest    
+    file_name = "create_localized_strings"
+    @migration_name = file_name.camelize
+    
+    record do |m|
+      m.migration_template "localizer_migration.rb.erb",
+                           File.join('db', 'migrate'),
+                           :migration_file_name => file_name
+    end
+  end 
+  
+end
